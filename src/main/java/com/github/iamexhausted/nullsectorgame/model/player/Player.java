ebee6d9ef93a0a18902da84b31d5b3da76afe858
exhausted <sikacmak@cvut.cz>
2026-07-15 14:03:00 +0200

    feat: added Player entity

---
+++ src/main/java/com/github/iamexhausted/nullsectorgame/model/player/Player.java
@@ -0,0 +1,23 @@
+package com.github.iamexhausted.nullsectorgame.model.player;
+
+import com.github.iamexhausted.nullsectorgame.model.entity.Entity;
+import com.github.iamexhausted.nullsectorgame.model.image_system.ImgId;
+
+public class Player extends Entity {
+    // speed
+    private double vx = 0;
+    private double vy = 0;
+
+    public Player(ImgId imgId, double x, double y) {
+        final int MAX_HEALTH = 100;
+        final int DAMAGE = 15;
+
+        super(imgId, x, y, MAX_HEALTH, DAMAGE); // hp = 100, damage = 15
+
+        this.setHitboxScale(0.45, 1.0);
+        this.setHitboxOffset(8, 0);
+    }
+
+    @Override
+    public void update(double deltaTime) {}
+}
