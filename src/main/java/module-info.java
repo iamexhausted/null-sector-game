c9d326755c16ed3df6e9bdd0af1833f988af9225
exhausted <sikacmak@cvut.cz>
2026-07-15 13:17:32 +0200

    build: added Gson dependency to pom.xml and updated module-info.java accessibility and dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module com.github.iamexhausted.nullsectorgame.nullsectorgit {
     requires javafx.controls;
+    requires com.google.gson;
+
+    opens com.github.iamexhausted.nullsectorgame.model.level_system to com.google.gson;
+    opens com.github.iamexhausted.nullsectorgame.model.dto to com.google.gson;
 
     exports com.github.iamexhausted.nullsectorgame;
 }
